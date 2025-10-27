8d85099cc30530304fe11f88d76b015a68dbdf8c
haohaoo78 <haohaao78@gmail.com>
2025-10-27 11:35:23 +0700

    Connect

--- config/database.js
+++ config/database.js
@@ -1,10 +1,11 @@
+require('dotenv').config(); 
 const mysql = require('mysql2/promise');
 
 const db = mysql.createPool({
   host: process.env.MYSQL_HOST || 'localhost',
   user: process.env.MYSQL_USER || 'root',
-  password: process.env.MYSQL_PASSWORD || 'Shatou5114',
-  database: process.env.MYSQL_DATABASE || 'ptud',
+  password: process.env.MYSQL_PASSWORD || 'password',
+  database: process.env.MYSQL_DATABASE || 'test',
   port: process.env.MYSQL_PORT || 3306,
   waitForConnections: true,
   connectionLimit: 10,
